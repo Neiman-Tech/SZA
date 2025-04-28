e439641ad26773f158c772d7fca539ced7d3a0de
engineermarcus <engineermarcus72@gmail.com>
2025-04-28 13:58:40 +0300

    @ll

---
+++ routes/userRoutes.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const User = require('../models/User');  
+const router = express.Router();
+
+router.post('/users', async (req, res) => {
+  try {
+    const { name, email, age } = req.body;
+    const user = new User({ name, email, age });
+    await user.save();
+    res.status(201).json(user); 
+  } catch (err) {
+    res.status(400).json({ error: err.message });  
+  }
+});
+
+
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);  
+  } catch (err) {
+    res.status(500).json({ error: err.message });  
+  }
+});
+
+router.put('/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, email, age } = req.body;
+    const updatedUser = await User.findByIdAndUpdate(id, { name, email, age }, { new: true });
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json(updatedUser);  
+  } catch (err) {
+    res.status(400).json({ error: err.message });  
+  }
+});
+
+router.delete('/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await User.findByIdAndDelete(id);
+    if (!deletedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json({ message: 'User deleted successfully' });  
+  } catch (err) {
+    res.status(400).json({ error: err.message }); 
+  }
+});
+
+module.exports = router;  
