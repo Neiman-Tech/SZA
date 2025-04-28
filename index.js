54870b95f23efe16d812c3d82d3cd67a98f6cc3d
engineermarcus <engineermarcus72@gmail.com>
2025-04-28 18:28:25 +0300

    sign up & login

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const userRoutes = require('./routes/userRoutes');  
+const path = require('path');
 
 const app = express();
+app.use(express.static('public'));
 const port = 5000;
 
 mongoose.connect(process.env.MONGO_URI)
@@ -15,6 +17,21 @@ app.use(express.json());
 
 
 app.use(userRoutes);
+app.get("/",(req,res) => {
+
+
+	res.sendFile(path.join(__dirname,'public','index.html'));
+
+});
+
+app.get('/signup', (req,res) => { 
+	res.sendFile(path.join(__dirname,'public','signup.html'));
+});
+app.get('/login',(req,res) => {
+	res.sendFile(path.join(__dirname,'public','login.html'));
+
+});
+
 
 
 app.listen(port, () => {
